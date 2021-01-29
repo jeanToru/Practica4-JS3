12fc1be11ac735b9b3d06450f9cd91f5dfb769ab
Jean Brown <jeantoruno00@gmail.com>
2021-01-28 17:52:14 -0800

    Validacion de Formulario

--- js/script.js
+++ js/script.js
@@ -24,9 +24,84 @@
 
 // BOILERPLATE
 const form = document.querySelector("form");
+const content = document.querySelector('.validacion');
+const warning = document.createElement('div');
+const check = document.createElement('p');
 
-form.addEventListener("submit", function(e) {
+const validation = () => {
+  const name = document.querySelector('input[type=name]');
+  const lastName = document.querySelector('input[type=text]');
+  const email = document.querySelector('input[type=email]');
+
+  if (name.value === '' && lastName.value === '' && email.value === '') {
+    name.classList.add('error');
+    lastName.classList.add('error');
+    email.classList.add('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica los siguientes campos:</p><ul><li>Nombre</li><li>Apellido</li><li>Email</li></ul>`;
+    content.appendChild(warning);
+
+  } else if (name.value === '' && lastName.value === '') {
+    name.classList.add('error');
+    lastName.classList.add('error');
+    email.classList.remove('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica los siguientes campos:</p><ul><li>Nombre</li><li>Apellido</li></ul>`;
+    content.appendChild(warning);
+  }
+
+  else if (email.value === '' && lastName.value === '') {
+    name.classList.remove('error');
+    lastName.classList.add('error');
+    email.classList.add('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica los siguientes campos:</p><ul><li>Apellido</li><li>Email</li></ul>`;
+    content.appendChild(warning);
+  }
+
+  
+  else if (name.value === '' && email.value === '') {
+    name.classList.add('error');
+    lastName.classList.remove('error');
+    email.classList.add('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica los siguientes campos:</p><ul><li>Nombre</li><li>Email</li></ul>`;
+    content.appendChild(warning);
+  }
+
+  else if (name.value === '') {
+    name.classList.add('error');
+    lastName.classList.remove('error');
+    email.classList.remove('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica el siguiente campo:</p><ul><li>Nombre</li></ul>`;
+    content.appendChild(warning);
+  }
+
+  else if (lastName.value === '') {
+    name.classList.remove('error');
+    lastName.classList.add('error');
+    email.classList.remove('error');
+    warning.innerHTML = `<p>Ocurrio un error, verifica el siguiente campo:</p><ul><li>Apellido</li></ul>`;
+    content.appendChild(warning);
+  }
+
+  else if (email.value === '') {
+    name.classList.remove('error');
+    lastName.classList.remove('error');
+    email.classList.add('error');
+    warning.innerHTML = `<p>Ocurrio un error,verifica el siguiente campo:</p><ul><li>Email</li></ul>`;
+    content.appendChild(warning);
+  }
+
+ else {
+    warning.innerHTML = '';
+    name.classList.remove('error');
+    lastName.classList.remove('error');
+    email.classList.remove('error');
+    check.innerHTML = 'Su formulario fue enviado';
+    check.classList.add('success');
+    form.appendChild(check);
+  }
+}
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-
+  validation();
   console.log("FORMULARIO ENVIADO");
 });
